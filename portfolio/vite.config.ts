5fbc76ea4a89b35230be2ece80a99bc4d9befff0
Logan Hindley <lohindley@gmail.com>
2026-01-01 15:21:02 -0800

    Pages prep

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => {
-// NOTE!
-  // - If your repo is EXACTLY username.github.io => base should be "/"
-  // - Otherwise => base should be "/REPO_NAME/"
-  // We let the GitHub Action pass BASE_PATH, but default to "/".
-  const base = process.env.BASE_PATH ?? "/";
-
-  return {
-    plugins: [react()],
-    base,
-  };
-});
+export default defineConfig(() => ({
+  plugins: [react()],
+  base: process.env.BASE_PATH ?? "/",
+}));
